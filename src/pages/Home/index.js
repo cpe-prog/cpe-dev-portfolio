5566099e6a665a4f022725aa419ac643d5a7071f
Grian Gajila <griangajila2@gmail.com>
2023-10-22 17:02:32 +0800

    feat: implement navigation page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,9 @@
+import NavBar from "@/layouts/NavBar";
+
 export default function Home() {
-	return <div className="bg-slate-600">Home Page</div>;
+	return (
+		<div className="bg-slate-600">
+			<NavBar />
+		</div>
+	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import Home from "./Home";
 
 export default function Main() {
-	return <Home />;
+	return (
+		<main className="p-0 m-0 w-auto h-auto">
+			<Home />
+		</main>
+	);
 }
