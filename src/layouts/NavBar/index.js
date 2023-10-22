e04f98f4e38d3d21b854ec104e71d9c9fe0af14a
Grian Gajila <griangajila2@gmail.com>
2023-10-22 17:22:09 +0800

    feat: implement navigation page

--- src/layouts/NavBar/index.js
+++ src/layouts/NavBar/index.js
@@ -1,3 +1,29 @@
+import Link from "next/link";
+
 export default function NavBar() {
-	return <div>Navigation</div>;
+	return (
+		<nav>
+			<Link href={"/"}>
+				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
+					Home
+				</span>
+			</Link>
+
+			<Link href={"/"}>
+				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
+					Portfolio
+				</span>
+			</Link>
+			<Link href={"/"}>
+				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
+					About
+				</span>
+			</Link>
+			<Link href={"/"}>
+				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
+					Contact
+				</span>
+			</Link>
+		</nav>
+	);
 }
