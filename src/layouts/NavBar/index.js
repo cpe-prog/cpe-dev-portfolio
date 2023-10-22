a54668404bf4eba6c5ab4529127637e18b08d7a4
Grian Gajila <griangajila2@gmail.com>
2023-10-22 18:14:42 +0800

    wip

--- src/layouts/NavBar/index.js
+++ src/layouts/NavBar/index.js
@@ -1,22 +1,35 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+	const router = useRouter();
+	const { pathname } = router;
+
+	const inactiveLink = "flex gap-2 p-2 rounded-lg hover:bg-gray-500";
+	const activeLink =
+		inactiveLink +
+		"flex gap-2 p-2 rounded-lg bg-gray-300 text-slate-700 font-medium hover:bg-gray-500 hover:text-white";
 	return (
-		<nav>
+		<nav className="">
 			<Link href={"/"}>
 				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
-					Home
+					Logo
+				</span>
+			</Link>
+			<Link className={pathname === "/" ? activeLink : inactiveLink} href={"/"}>
+				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
+					About Me
 				</span>
 			</Link>
 
 			<Link href={"/"}>
 				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
-					Portfolio
+					Skills
 				</span>
 			</Link>
 			<Link href={"/"}>
 				<span className="text-2xl text-white pr-4 border-solid border-r-2 border-s-fuchsia-800 border-white font-bold">
-					About
+					Projects
 				</span>
 			</Link>
 			<Link href={"/"}>
