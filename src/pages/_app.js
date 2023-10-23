a4a348738f5e2bf19a272db11e94ee725d7b975a
Grian Gajila <griangajila2@gmail.com>
2023-10-23 22:56:53 +0800

    wip

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Roboto } from "next/font/google";
+import Head from "next/head";
 
 const roboto = Roboto({
 	weight: "400",
@@ -8,8 +9,13 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }) {
 	return (
-		<main className={roboto.className}>
-			<Component {...pageProps} />;
-		</main>
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<main className={roboto.className}>
+				<Component {...pageProps} />;
+			</main>
+		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ module.exports = {
 		"./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		// extend: {
-		//   backgroundImage: {
-		//     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-		//     'gradient-conic':
-		//       'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-		//   },
-		// },
+		extend: {
+			colors: {
+				dark: "#1b1b1b",
+				light: "#f5f5f5",
+				primary: " #b63e96",
+				primaryDark: "#58e6d9",
+			},
+		},
 	},
 	plugins: [],
 };
