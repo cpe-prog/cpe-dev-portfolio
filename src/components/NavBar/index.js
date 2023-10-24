1b9ed7a211f3a529303cff6bb75dc9d14248e664
Grian Gajila <griangajila2@gmail.com>
2023-10-24 23:41:13 +0800

    wip

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,26 +4,28 @@ export default function NavBar() {
 	const inActiveLink = "hover:p-2  mr-8 p-2 Underline inline-block relative";
 	return (
 		<>
-			<header className=" w-full h-14 bg-slate-200 flex items-center justify-between font-medium text-gray-700">
-				<nav>
-					<Link className="font-extrabold unde text-gray-700 pl-9" href="/">
-						Cpe-Prog.
-					</Link>
-				</nav>
-				<nav>
-					<Link className={inActiveLink} href="/About">
-						About Me
-					</Link>
-					<Link className={inActiveLink} href="/Skills">
-						Skills
-					</Link>
-					<Link className={inActiveLink} href="/Projects">
-						Projects
-					</Link>
-					<Link className={inActiveLink} href="/Contact">
-						Contact
-					</Link>
-				</nav>
+			<header className=" w-full bg-slate-900 h-20 flex items-center justify-center font-medium text-gray-700">
+				<div className="bg-slate-200 p-1 w-[50%] flex items-center justify-between rounded-lg">
+					<nav className="mr-20">
+						<Link className="font-extrabold text-gray-700 pl-9" href="/">
+							Cpe-Prog.
+						</Link>
+					</nav>
+					<nav>
+						<Link className={inActiveLink} href="/About">
+							About Me
+						</Link>
+						<Link className={inActiveLink} href="/Skills">
+							Skills
+						</Link>
+						<Link className={inActiveLink} href="/Projects">
+							Projects
+						</Link>
+						<Link className={inActiveLink} href="/Contact">
+							Contact
+						</Link>
+					</nav>
+				</div>
 			</header>
 		</>
 	);
