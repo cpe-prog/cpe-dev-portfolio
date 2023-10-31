210fbf8c9ea4b0628a714ef28e41af21d0b98ce6
Grian Gajila <griangajila2@gmail.com>
2023-11-01 02:37:55 +0800

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,12 @@ export default function Home() {
 			</Head>
 			<main className="flex items-center text-textDark w-full min-h-screen">
 				<Layout className="pt-0">
-					<div className="flex items-center justify-between w-full">
+					<div className="flex items-center justify-around w-full">
+						<div>
+							<h1>Hello I'm Grian Gajila</h1>
+							<h2>UI/UX designer Responsive Web Design</h2>
+							<p>Aspiring Senior Full Stack Web Developer.</p>
+						</div>
 						<div className=" rounded-tr-3xl rounded-br-full rounded-tl-full rounded-bl-full bg-gradient-to-tl from-slate-800 via-cyan-300 to-slate-700 background-animate">
 							<div className="rounded-3xl rounded-tl-full rounded-br-full rounded-tr-full bg-gradient-to-tl from-slate-800 via-cyan-300 to-slate-700 background-animate">
 								<Image
