7d57622193b443f7f011b7c7e5ec20a6c31c532e
Grian Gajila <griangajila2@gmail.com>
2023-11-02 16:49:32 +0800

    wip

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,15 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Link from "next/link";
-
+import { useRouter } from "next/router";
 import { ThemeMode } from "../icons/icon";
 
 export default function NavBar() {
+	const router = useRouter();
+	const { pathname } = router;
 	const inActiveLink =
-		"hover:p-2 hover:text-textHoverDark mr-8 p-2 Underline text- inline-block relative";
+		"hover:p-2 hover:text-textHoverDark mr-8 p-2 inActiveLink inline-block relative text-textDark ";
+	const activeLink = inActiveLink + "hover:p-2 text-textHoverDark mr-8 p-2";
+
 	return (
 		<>
 			<header className=" w-full bg-bgLight drop-shadow-xl shadow-black fixed h-16 flex items-center justify-around fontColor">
@@ -17,18 +21,30 @@ export default function NavBar() {
 						Cpe-Prog.
 					</Link>
 				</nav>
-				<nav className="p-2  text-textDark ">
+				<nav>
 					<div>
-						<Link className={inActiveLink} href="/About">
+						<Link
+							className={pathname === "/About" ? activeLink : inActiveLink}
+							href={"/About"}
+						>
 							About Me
 						</Link>
-						<Link className={inActiveLink} href="/Skills">
+						<Link
+							className={pathname === "/Skills" ? activeLink : inActiveLink}
+							href={"/Skills"}
+						>
 							Skills
 						</Link>
-						<Link className={inActiveLink} href="/Projects">
+						<Link
+							className={pathname === "/Projects" ? activeLink : inActiveLink}
+							href={"/Projects"}
+						>
 							Projects
 						</Link>
-						<Link className={inActiveLink} href="/Contact">
+						<Link
+							className={pathname === "/Contact" ? activeLink : inActiveLink}
+							href={"/Contact"}
+						>
 							Contact
 						</Link>
 					</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
 				bgLight: "#f0fcf8",
 				textLight: "#b8bcc6",
 				textDark: "#5a5f7a",
-				textHoverDark: "#414558",
+				textHoverDark: "#313442",
+				activeBorderBottom: "#69d3a7",
 				primary: " #b63e96",
 				primaryDark: "#58e6d9",
 				acuaGreen: {
